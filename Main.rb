6ae0604043402e8b2b6ae1d6c926a20fe598a5f7
Will <teck.geek@hotmail.com>
2014-10-17 17:59:33 -0600

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -1,9 +1,8 @@
 include Math
 class Main
- def Fibonacci n
+def fibonacci( n )
   return  n  if ( 0..1 ).include? n
   ( Fibonacci( n - 1 ) + Fibonacci( n - 2 ) )
-    puts Fibonacci( 5 )
 end
 puts "Please enter the nth number for the Fibanachi Sequence"
 n = gets.chomp 
@@ -12,13 +11,18 @@ puts "Starting Formula"
   puts a.to_s
   puts "Finished Formula"
   puts "Starting Induction"
-  x=1
-      y=1
-      while(n.to_i >-1)
-        y=x+y
-        puts y.to_s
-        n = n-1
-      end
-      puts y
-  Main.Fibonacci n
+  prevVal=1
+  prevPrevVal=1
+  m= n.to_i
+  do
+    currVal = prevVal + prevPrevVal;
+    prevPrevVal = prevVal;
+    prevVal = currVal;
+    m++;
+  while(m < n)
+  puts y.to_s
+  puts "Finished Induction"
+  puts"Starting Recursion"
+  puts fibonacci( n )
+  puts"Finished Recursion"
 end
