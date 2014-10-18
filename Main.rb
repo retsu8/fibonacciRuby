5fc107d4d33807e15e7788132ef470314ded8013
Will <teck.geek@hotmail.com>
2014-10-17 19:21:37 -0600

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -7,13 +7,17 @@ class Main
 puts "Please enter the nth number for the Fibanachi Sequence"
 n = gets.chomp 
 puts "Starting Formula"
+beginning_time = Time.now
   PartA = 1/Math.sqrt(5)
   PartB = ((1+Math.sqrt(5))/2)**(n.to_i+1)
   PartC = ((1-Math.sqrt(5))/2)**(n.to_i+1)
   a = PartA*PartB - PartA*PartC
   puts a.to_s
+  end_time = Time.now
   puts "Finished Formula"
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   puts "Starting Induction"
+  beginning_time = Time.now
   prevVal=1
   prevPrevVal=0
   m=n.to_i
@@ -24,8 +28,13 @@ puts "Starting Formula"
     m = m-1
   end
   puts currVal.to_s
+  end_time = Time.now
   puts "Finished Induction"
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   puts"Starting Recursion"
+  beginning_time = Time.now
   puts fibonacci( n.to_i )
+  end_time = Time.now
+  puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
   puts"Finished Recursion"
 end
