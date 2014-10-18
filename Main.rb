c5e0adf52707dbfb493865f18afa4a042a926cd8
Will <teck.geek@hotmail.com>
2014-10-17 18:54:28 -0600

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -7,20 +7,23 @@ end
 puts "Please enter the nth number for the Fibanachi Sequence"
 n = gets.chomp 
 puts "Starting Formula"
-  a = ((13)**n.to_i-(12)**n.to_i)/2
+  PartA = 1/Math.sqrt(5)
+  PartB = ((1+Math.sqrt(5))/2)**(n.to_i+1)
+  PartC = ((1-Math.sqrt(5))/2)**(n.to_i+1)
+  a = PartA*PartB - PartA*PartC
   puts a.to_s
   puts "Finished Formula"
   puts "Starting Induction"
   prevVal=1
-  prevPrevVal=1
-  m= n.to_i
-  do
-    currVal = prevVal + prevPrevVal;
-    prevPrevVal = prevVal;
-    prevVal = currVal;
-    m++;
-  while(m < n)
-  puts y.to_s
+  prevPrevVal=0
+  m=n.to_i
+  while m > 0
+    currVal = prevVal + prevPrevVal
+    prevPrevVal = prevVal
+    prevVal = currVal
+    m = m-1
+  end
+  puts currVal.to_s
   puts "Finished Induction"
   puts"Starting Recursion"
   puts fibonacci( n )
